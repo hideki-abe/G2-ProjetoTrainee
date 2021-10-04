0faf84114e44ecebbce81a8ee66ff82004300ffe
hideki <hideki-abe@hotmail.com>
2021-10-04 15:29:57 -0300

    Backend

---
+++ Backend/g2/src/main/java/com/projeto/g2/G2Application.java
@@ -0,0 +1,13 @@
+package com.projeto.g2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G2Application.class, args);
+	}
+
+}
---
+++ Backend/g2/src/test/java/com/projeto/g2/G2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.g2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
