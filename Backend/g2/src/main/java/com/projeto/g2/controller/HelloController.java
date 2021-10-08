be6d6a8e04000d47213ad8e61cbff5e538cb3780
hideki <hideki-abe@hotmail.com>
2021-10-07 22:44:41 -0300

    testes no Postman

--- Backend/g2/src/main/java/com/projeto/g2/controller/HelloController.java
+++ Backend/g2/src/main/java/com/projeto/g2/controller/HelloController.java
@@ -3,8 +3,10 @@ package com.projeto.g2.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RestController
 public class HelloController {
 	
 	@RequestMapping("/")
